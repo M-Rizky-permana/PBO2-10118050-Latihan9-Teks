6e7bcd6005a2be4e978d9e76f797939d7deedfcc
M-Rizky-permana <55745444+M-Rizky-permana@users.noreply.github.com>
2019-09-26 20:04:20 +0700

    Add files via upload

---
+++ PBO210118050Latihan9Teks/src/pbo210118050latihan9teks/PBO210118050Latihan9Teks.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo210118050latihan9teks;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhamad Rizky Permana
+ * KELAS    : IF-2
+ * NIM      : 10118050
+ * Deskripsi Program : Menampilkan Teks
+ */
+public class PBO210118050Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        String tab = "\t";
+        String newline = "\n";
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char :" + tab + "berisi " + c);
+        
+        
+    }
+    
+}
